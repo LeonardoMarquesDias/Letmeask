0a51af37ca1f274357f3f59a1ee07276405b20fb
LeonardoDias <leonardo_marquesdias@hotmail.com>
2021-06-23 02:17:29 +0100

    Env.local + Firebase connect

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,13 +5,13 @@ import 'firebase/database';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQU5arB5009GFJytgLxNeGF_-tDkLQ0lU",
-  authDomain: "letmeask-499a3.firebaseapp.com",
-  databaseURL: "https://letmeask-499a3-default-rtdb.firebaseio.com",
-  projectId: "letmeask-499a3",
-  storageBucket: "letmeask-499a3.appspot.com",
-  messagingSenderId: "415717292895",
-  appId: "1:415717292895:web:d17843de39e832e42954ea"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
